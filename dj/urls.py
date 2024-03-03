4a5f08fdd609482e1d7b3db1f49142abb7c09d86
Derrell Jones <70511199+derrellj@users.noreply.github.com>
2024-03-03 11:41:38 +0300

    Fixed error hopfully

--- dj/urls.py
+++ dj/urls.py
@@ -4,18 +4,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("rachel-nails-admin/", admin.site.urls),
+    path("admin/", admin.site.urls),
     path("", include("pages.urls")),
     path("about/", include("about.urls")),
     path("location/", include("location.urls")),
     path("service/", include("services.urls")),
 ]
 if settings.DEBUG:
-    import debug_toolbar
-
-    urlpatterns = [
-        path("__debug__/", include(debug_toolbar.urls)),
-    ] + urlpatterns
 
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
